8ac34b7e1e69c51d680e0616932e7974982d3888
1Alexander1-oss <kolesnikov.1994@bk.ru>
2022-03-14 22:14:47 +0300

    add arithmetic

--- app/src/main/java/com/nyx/klschildgame/ArithmeticActivity.kt
+++ app/src/main/java/com/nyx/klschildgame/ArithmeticActivity.kt
@@ -2,11 +2,15 @@ package com.nyx.klschildgame
 
 import android.os.Bundle
 import android.widget.GridView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class ArithmeticActivity : AppCompatActivity() {
     private lateinit var leftListView: GridView
     private lateinit var rightListView: GridView
+    private lateinit var leftTerm: TextView
+    private lateinit var rightTerm: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,15 +18,34 @@ class ArithmeticActivity : AppCompatActivity() {
 
         leftListView = findViewById(R.id.left_list_view)
         rightListView = findViewById(R.id.right_list_view)
+        leftTerm = findViewById(R.id.left_term)
+        rightTerm = findViewById(R.id.right_term)
 
 
+        val firstTerm = getRandomTerm()
+        val secondTerm = getRandomTerm()
 
-        val adapter = ArithmeticImageItemAdapter(
+        val leftTermAdapter = ArithmeticImageItemAdapter(
             this,
-            listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18),
+            firstTerm,
             R.drawable.turtle
         )
-        leftListView.adapter = adapter
-        rightListView.adapter = adapter
+
+        val rightTermAdapter = ArithmeticImageItemAdapter(
+            this,
+            secondTerm,
+            R.drawable.turtle
+        )
+
+        leftListView.adapter = leftTermAdapter
+        rightListView.adapter = rightTermAdapter
+
+        leftTerm.text = firstTerm.toString()
+        rightTerm.text = secondTerm.toString()
     }
+
+    private fun getRandomTerm(): Int {
+        return Random.nextInt(1, 6)
+    }
+
 }
--- app/src/main/java/com/nyx/klschildgame/ArithmeticImageItemAdapter.kt
+++ app/src/main/java/com/nyx/klschildgame/ArithmeticImageItemAdapter.kt
@@ -9,14 +9,14 @@ import android.widget.ImageView
 
 class ArithmeticImageItemAdapter(
     private val context: Context,
-    private val items: List<Int>,
+    private val imagesCount: Int,
     private val imageResId: Int,
 ) : BaseAdapter() {
-    override fun getCount(): Int = items.size
+    override fun getCount(): Int = imagesCount
 
-    override fun getItem(position: Int) = items[position]
+    override fun getItem(position: Int) = imagesCount
 
-    override fun getItemId(position: Int) = items[position].toLong()
+    override fun getItemId(position: Int) = imagesCount.toLong()
 
     override fun getView(position: Int, p1: View?, parent: ViewGroup?): View {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
